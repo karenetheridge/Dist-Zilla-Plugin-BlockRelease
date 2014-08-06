cc44ec7eb76af223b76b5d46dcf5d0d4f20f4799
Karen Etheridge <ether@cpan.org>
2014-08-05 23:28:40 -0700

    add keywords to metadata

--- lib/Dist/Zilla/Plugin/BlockRelease.pm
+++ lib/Dist/Zilla/Plugin/BlockRelease.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::BlockRelease;
 # ABSTRACT: Prevent a release from occurring
+# KEYWORDS: plugin distribution release sanity safety prevent
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
