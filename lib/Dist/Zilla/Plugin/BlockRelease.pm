e9f6204f46942a721a95388b082f0dc03f82e255
Karen Etheridge <ether@cpan.org>
2015-08-21 17:02:44 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/BlockRelease.pm
+++ lib/Dist/Zilla/Plugin/BlockRelease.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::BlockRelease;
 # ABSTRACT: Prevent a release from occurring
 # KEYWORDS: plugin distribution release sanity safety prevent
-# vim: set ts=8 sts=4 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.003';
 
