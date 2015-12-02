d7a9cdfda5c0d927ea579612e461483e1f84cf66
Karen Etheridge <ether@cpan.org>
2015-12-01 19:06:35 -0800

    increment $VERSION after 0.003 release

--- lib/Dist/Zilla/Plugin/BlockRelease.pm
+++ lib/Dist/Zilla/Plugin/BlockRelease.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::BlockRelease;
 # KEYWORDS: plugin distribution release sanity safety prevent
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 use Moose;
 with 'Dist::Zilla::Role::BeforeRelease',
